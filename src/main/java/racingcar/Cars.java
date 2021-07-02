ccb694576fa54c8cc27bfa048b93e08d2e6d895f
Hyun-juhee <juhe1998@naver.com>
2021-07-02 15:22:51 +0900

    feat(Cars): 모든 자동차들은 값에 맞게 이동한다.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -16,6 +16,15 @@ public class Cars {
     public static Cars of(List<Car> cars) {
         return new Cars(cars);
     }
+    public List<Car> getCars() {
+        return cars;
+    }
+    public void move(List<Integer> values) {
+        validateValuesCount(values);
+        for (int i = 0; i < cars.size(); i ++) {
+            cars.get(i).move(values.get(i));
+        }
+    }
     private void validateCarCount(List<Car> cars) {
         if (cars.size() < MIN_CARS_COUNT) {
             throw new IllegalArgumentException("자동차는 두 대 이상이어야 합니다.");
@@ -27,6 +36,11 @@ public class Cars {
         Set<String> carsNameSet = new HashSet<>(carsNameList);
         if (carsNameSet.size() != carsNameList.size()) {
             throw new IllegalArgumentException("자동차는 중복된 이름을 가질 수 없습니다.");
+        }
+    }
+    private void validateValuesCount(List<Integer> values) {
+        if (cars.size() != values.size()) {
+            throw new IllegalArgumentException("값의 개수와 자동차의 개수가 같아야 합니다. ");
         }
     }
 }
--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarsTest {
@@ -19,12 +21,55 @@ public class CarsTest {
 
     @Test
     @DisplayName("중복된 자동차 이름 있을 시 IllegalArgumentException 발생 확인")
-    public void carsSholdNotDuplicated() {
+    public void carsShouldNotDuplicatedTest() {
         List<Car> cars = new ArrayList<>();
         cars.add(Car.of("juhi"));
         cars.add(Car.of("juhi"));
         assertThrows(IllegalArgumentException.class, ()-> {
             Cars.of(cars);
+        });
+    }
+
+    @Test
+    @DisplayName("자동차들이 정상적으로 움직이는지 확인")
+    public void carsMoveTest() {
+        List<Car> carsList = new ArrayList<>();
+        carsList.add(Car.of("Juhi"));
+        carsList.add(Car.of("Eric"));
+        carsList.add(Car.of("Tom"));
+        Cars cars = Cars.of(carsList);
+
+        List<Integer> values = new ArrayList<>();
+        values.add(4);
+        values.add(1);
+        values.add(5);
+
+        List<Integer> expectedPositions = new ArrayList<>();
+        expectedPositions.add(1);
+        expectedPositions.add(0);
+        expectedPositions.add(1);
+
+        cars.move(values);
+        for (int i = 0; i < 3; i++){
+            assertEquals(cars.getCars().get(i).getPosition(), expectedPositions.get(i));
+        }
+    }
+
+    @Test
+    @DisplayName("자동차 이동 시 자동차의 개수와 다른 개수의 리스트가 들어왔을 대 IllegalArgumentException 발생 확인")
+    public void moveValueShouldSameWithCarsCountTest() {
+        List<Car> carsList = new ArrayList<>();
+        carsList.add(Car.of("Juhi"));
+        carsList.add(Car.of("Eric"));
+        carsList.add(Car.of("Tom"));
+        Cars cars = Cars.of(carsList);
+
+        List<Integer> values = new ArrayList<>();
+        values.add(4);
+        values.add(1);
+
+        assertThrows(IllegalArgumentException.class, ()-> {
+            cars.move(values);
         });
     }
 }
