82a8f24998a0fd2db7906e3e6a4600468182d105
Hyun-juhee <juhe1998@naver.com>
2021-07-08 22:57:36 +0900

    refactor: domain 디렉토리 변경

--- src/test/java/racingcar/RoundTest.java
+++ src/test/java/racingcar/RoundTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Round;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
